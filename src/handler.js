023120c2a19cf1751eddf1a530b3915f06e4b9e0
Singgih Pratama <singgih.vendor.pasti@pegadaian.co.id>
2023-11-04 18:25:17 +0700

    develop getAllBookHandler

--- src/handler.js
+++ src/handler.js
@@ -15,11 +15,13 @@ const addBookHandler = (request, h) => {
     } = request.payload;
 
     const finished = pageCount === readPage;
-    const bookId = nanoid(16);
+    const id = nanoid(16);
     const insertedAt = new Date().toISOString();
     const updatedAt = insertedAt;
 
-    const newBooks = {name,
+    const newBooks = {
+        id,
+        name,
         year, 
         author, 
         summary, 
@@ -28,7 +30,6 @@ const addBookHandler = (request, h) => {
         readPage,
         finished,
         reading,
-        bookId,
         insertedAt,
         updatedAt
     };
@@ -63,20 +64,46 @@ const addBookHandler = (request, h) => {
                 status: "success",
                 message: "Buku berhasil ditambahkan",
                 data: {
-                    bookId : bookId,
+                    id : id,
                 },
             });
             response.code(201);
+            logger.info("membuat buku: ", newBooks)
             logger.info("End proses membuat buku", {
-                bookId: newBooks.bookId
+                id: newBooks.id
             })
             return response;
         }
     } catch (error) {
-        logger.error("Terjadi error:", error);
+        logger.error("Terjadi error: ", error);
         throw error;
     }
 }
 
+const getAllBookHandler = (_, h) => {
+    logger.info("Start proses menampilkan seluruh buku")
+    try {
+        const newBooks = books.map(book => ({
+            id: book.id,
+            name: book.name,
+            publisher: book.publisher
+            }))
 
-module.exports = {addBookHandler}
+        const response = h.response({
+            status: "success",
+            data: {
+                newBooks
+            },
+        })
+        response.code(200);
+        logger.info("menampilkan buku: ", newBooks)
+        logger.info("End proses menampilkan seluruh buku", newBooks.id)
+        return response;
+    } catch(error) {
+        logger.error("Terjadi error: ", error)
+        throw error;
+    }
+};
+
+
+module.exports = {addBookHandler, getAllBookHandler}
